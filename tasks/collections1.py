0a2ae745f43a6af0a5ca8dd15f9e704a79feba96
Marije-Kouyzer <marijekouyzer95@gmail.com>
2020-12-16 15:25:32 +0100

    new version of collections1.py

--- tasks/collections1.py
+++ tasks/collections1.py
@@ -7,17 +7,13 @@ import collections
 
 def get_word_counts(docs):
 
-    """
-    use the Collections python standard librar to count the words in the documents
-    docs = [[w1,w2], [w4,w2,w3,w1] ....]
+	"""
+	use the Collections python standard librar to count the words in the documents
+	docs = [[w1,w2], [w4,w2,w3,w1] ....]
 
-    restrictions:
-    1) you have to use Collections
-    2) you have 4 lines of code, including return statement
-    """
+	restrictions:
+	1) you have to use Collections
+	2) you have 4 lines of code, including return statement
+	"""
 
-    return collections.Counter([w for doc in docs for w in doc])
-
-if __name__ == "__main__":
-    docs = [['w1','w2'], ['w4','w2','w3','w1']]
-    print(get_word_counts(docs))
+	return collections.Counter([w for doc in docs for w in doc])
